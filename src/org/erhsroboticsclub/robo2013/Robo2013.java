b5ac9a1e97db445641461c66d2fdaadc48a8588c
Nick Yaculak <nyaculak@gmail.com>
2012-11-29 17:34:52 -0500

    first commit

---
+++ src/org/erhsroboticsclub/robo2013/Robo2013.java
@@ -0,0 +1,39 @@
+package org.erhsroboticsclub.robo2013;
+
+import org.erhsroboticsclub.robo2013.utilties.Messager;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+
+public class Robo2013 extends IterativeRobot {
+    
+    Messager msg;
+    
+    RobotDrive drive;
+    Joystick leftDriveStick, rightDriveStick, controlStick;
+    
+
+    public void robotInit() {
+        msg = new Messager();
+        msg.printLn("Loading Robot Code - Please Wait...");
+        
+        drive = new RobotDrive(RoboMap.TOP_LEFT_MOTOR, RoboMap.BOTTOM_LEFT_MOTOR,
+                               RoboMap.TOP_RIGHT_MOTOR, RoboMap.BOTTOM_RIGHT_MOTOR);
+        leftDriveStick = new Joystick(RoboMap.LEFT_DRIVE_STICK);
+        rightDriveStick = new Joystick(RoboMap.RIGHT_DRIVE_STICK);
+        controlStick = new Joystick(RoboMap.CONTROL_STICK);
+
+        msg.printLn("Done Loading: FRC 2013");
+    }
+    
+    public void teleopPeriodic() {
+        
+        drive.tankDrive(leftDriveStick, rightDriveStick);
+        
+    }
+
+    public void autonomousPeriodic() {
+
+    }
+    
+}
---
+++ src/org/erhsroboticsclub/robo2013/RoboMap.java
@@ -0,0 +1,20 @@
+package org.erhsroboticsclub.robo2013;
+
+public class RoboMap {
+    
+    /* Digital Output */
+    public static final int TOP_LEFT_MOTOR = 1;
+    public static final int BOTTOM_LEFT_MOTOR = 2;
+    public static final int TOP_RIGHT_MOTOR = 3;
+    public static final int BOTTOM_RIGHT_MOTOR = 4;
+    
+    /* Analog */
+    public static final int GYRO = 1;
+    public static final int ULTRASONIC = 2;
+    
+    /* USB Input */
+    public static final int LEFT_DRIVE_STICK = 1;
+    public static final int RIGHT_DRIVE_STICK = 2;
+    public static final int CONTROL_STICK = 3;
+    
+}
