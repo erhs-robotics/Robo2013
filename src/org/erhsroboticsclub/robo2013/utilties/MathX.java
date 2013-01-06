2aad74f4a7bbf61c602fcc5a893a26cf9d9a4f8a
Nick Yaculak <nyaculak@gmail.com>
2013-01-06 00:15:38 -0500

    updating MathX class for max and min functions

--- src/org/erhsroboticsclub/robo2013/utilties/MathX.java
+++ src/org/erhsroboticsclub/robo2013/utilties/MathX.java
@@ -63,4 +63,18 @@ public class MathX {
     public static double sqrt(double d) {
         return java.lang.Math.sqrt(d);
     }
+    
+    public static double max(double d1, double d2) {
+        if (d1 > d2) {
+            return d1;
+        }
+        return d2;
+    }
+    
+    public static double min(double d1, double d2) {
+        if (d1 < d2) {
+            return d1;
+        }
+        return d2;
+    }
 }
