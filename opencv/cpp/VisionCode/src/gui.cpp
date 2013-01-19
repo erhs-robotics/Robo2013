59773f17e160de06cce925a23e0e58a58c6b977f
Michael Stevens <mls0.5mv2@gmail.com>
2013-01-18 21:46:03 -0500

    adding cpp code

---
+++ opencv/cpp/VisionCode/src/gui.cpp
@@ -0,0 +1,37 @@
+#include <cv.h>
+#include <highgui.h>
+#include <cstdio>
+using namespace cv;
+// A Simple Camera Capture Framework
+int main() {
+    CvCapture* capture = cvCaptureFromCAM ( CV_CAP_ANY );
+    if ( !capture ) {
+        fprintf ( stderr, "ERROR: capture is NULL \n" );
+        getchar();
+        return -1;
+    }
+    
+    cvNamedWindow ( "mywindow", CV_WINDOW_AUTOSIZE );
+    
+    while (true) {
+        
+        IplImage* frame = cvQueryFrame ( capture );
+        IplImage* imgHSV = cvCreateImage(cvGetSize(frame), 8, 3);
+        IplImage* imgThreshed = cvCreateImage(cvGetSize(frame), 8, 1);
+        cvCvtColor(frame, imgHSV, CV_BGR2HSV);
+        cvInRangeS(imgHSV, cvScalar(20, 100, 100), cvScalar(30, 255, 255), imgThreshed);
+        if ( !frame ) {
+            fprintf ( stderr, "ERROR: frame is null...\n" );
+            getchar();
+            break;
+        }
+        cvShowImage ( "mywindow", imgThreshed );
+        
+        //if Esc pressed
+        if ( ( cvWaitKey ( 10 ) & 255 ) == 27 ) break;
+    }
+    
+    cvReleaseCapture ( &capture );
+    cvDestroyWindow ( "mywindow" );
+    return 0;
+}
