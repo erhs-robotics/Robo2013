82f0d8dc314268b1c66373c9420eefcedc3ac5d3
Michael Stevens <mls0.5mv2@gmail.com>
2013-01-28 22:33:07 -0500

    adding get_max_rect function to imgproc and implementing Kinect class

--- opencv/python/Main.py
+++ opencv/python/Main.py
@@ -4,9 +4,8 @@ sys.path.append('lib')
 import cv2 
 from cv2 import cv
 import numpy as np
-from freenect import sync_get_depth as get_depth, sync_get_video as get_video
-from math import tan
 from imgproc import *
+from Kinect import Kinect
 
 imgproc = imgproc(-1)
 
@@ -22,27 +21,24 @@ cv2.namedWindow('Depth and RGB')
 cv.CreateTrackbar("X", 'Depth and RGB', x_pos, 632, update_x)
 cv.CreateTrackbar("Y", 'Depth and RGB', y_pos, 479, update_y)
 
-while True:
+kinect = Kinect()
 
-    depth,_ = get_depth()
-    rgb,_ = get_video()
+while True:
     
-    bgr = cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR)
+    bgr = kinect.get_video()
     rects, rects_img = imgproc.doImgProc(bgr)
-    if len(rects) > 0:
-        max_rect = rects[0]
-        for i in range(len(rects)):
-            if rects[i].area > max_rect.area:
-                max_rect = rects[i]
-                
+    
+    max_rect = imgproc.getMaxRect(rects)
+    if max_rect:
         print max_rect.x, max_rect.y
-        #x_pos = max_rect.x
-        #y_pos = max_rect.y
+        x_pos = max_rect.x
+        y_pos = max_rect.y
+    else:
+        print "No Taget Found!"
     
-    cv2.circle(rgb, (x_pos, y_pos), 2, (255,255,0), 5)        
+    cv2.circle(rgb, (x_pos, y_pos), 2, (255,255,0), 5)   
     
-    raw_depth_val = depth[y_pos][x_pos]
-    distance = 0.1236 * tan(raw_depth_val / 2842.5 + 1.1863) #raw->meters
+    distance = kinect.get_depth_at(x_pos, y_pos)
     print distance * 100 / 2.54# meters->feet->in
     
     # Build a two panel color image
