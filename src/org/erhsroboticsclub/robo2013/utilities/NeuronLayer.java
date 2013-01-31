61f3654bc8759ba82c58eb35f099b4f6f77d6ef5
David <davidofthedragons@gmail.com>
2013-01-31 15:57:17 -0500

    Added neural network code

---
+++ src/org/erhsroboticsclub/robo2013/utilities/NeuronLayer.java
@@ -0,0 +1,41 @@
+package neuralNet;
+
+public class NeuronLayer {
+
+	private double[][] weights; //to next layer 
+	private double inputs[];
+	private double outputs[];
+	private int neurons;
+	
+	public NeuronLayer(int neurons, int inputs) {
+		this.inputs = new double[inputs+1];  //With bias
+		this.inputs[0] = 1;
+		this.outputs = new double[neurons];
+		this.weights = new double[neurons][inputs+1];
+		this.neurons=neurons;
+	}
+	
+	public void calcOutputs() {
+		for(int i=0; i<neurons; i++) {
+			double total=0;
+			for(int j=0; j<inputs.length; i++) {
+				total += inputs[j] * weights[i][j];
+			}
+			outputs[i] = Utils.sigmoid(total);
+		}
+	}
+	
+	public double[] getInputs() {return inputs;}
+	public void setInputs(double[] in) {inputs = in;}
+	public void setInput(double in, int index) {inputs[index] = in;}
+	
+	public double[] getOutputs() {return outputs;}
+	public double getOutput(int i) {return outputs[i];}
+	
+	public double[][] getWeights() {return weights;}
+	public void setWeights(double[][] w) {weights = w;}
+	public void setWeight(double w, int i, int j) {weights[i][j] = w;}
+	public double getWeight(int i, int j) {return weights[i][j];}
+	
+	
+}
