823c1f476e14e8f8507fa3e67d4abc96b5cc42ba
Michael Stevens <mls0.5mv2@gmail.com>
2013-02-02 17:45:29 -0500

    adding socket test

--- src/org/erhsroboticsclub/robo2013/Robo2013.java
+++ src/org/erhsroboticsclub/robo2013/Robo2013.java
@@ -6,25 +6,27 @@ import edu.wpi.first.wpilibj.camera.AxisCamera;
 import edu.wpi.first.wpilibj.camera.AxisCameraException;
 import edu.wpi.first.wpilibj.can.CANTimeoutException;
 import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import javax.microedition.io.Connector;
+import javax.microedition.io.ServerSocketConnection;
 import javax.microedition.io.SocketConnection;
 import org.erhsroboticsclub.robo2013.imaging.ImageProcessing;
 import org.erhsroboticsclub.robo2013.utilities.Controls;
 import org.erhsroboticsclub.robo2013.utilities.Messenger;
 
 public class Robo2013 extends IterativeRobot {
-    
+
     RobotDrive drive;
     Joystick sticky;
     CANJaguar TOP_LEFT_JAGUAR, BOTTOM_LEFT_JAGUAR, TOP_RIGHT_JAGUAR, BOTTOM_RIGHT_JAGUAR;
     Messenger msg;
     Controls driveControls;
     Com com;
-    
-    
+
     public void robotInit() {
         //com = new Com("http://10.0.53.23/");
         msg = new Messenger();
@@ -33,9 +35,9 @@ public class Robo2013 extends IterativeRobot {
             TOP_LEFT_JAGUAR = new CANJaguar(RoboMap.TOP_LEFT_MOTOR);
             BOTTOM_LEFT_JAGUAR = new CANJaguar(RoboMap.BOTTOM_LEFT_MOTOR);
             TOP_RIGHT_JAGUAR = new CANJaguar(RoboMap.TOP_RIGHT_MOTOR);
-            BOTTOM_RIGHT_JAGUAR = new CANJaguar(RoboMap.BOTTOM_RIGHT_MOTOR); 
-            
-            
+            BOTTOM_RIGHT_JAGUAR = new CANJaguar(RoboMap.BOTTOM_RIGHT_MOTOR);
+
+
         } catch (CANTimeoutException ex) {
             ex.printStackTrace();
         }
@@ -43,27 +45,40 @@ public class Robo2013 extends IterativeRobot {
         sticky = new Joystick(1);
         driveControls = new Controls(sticky);
         msg.printLn("Done Loading: FRC 2013");
-        
-        while(true) {
+
+        while (true) {
             try {
-                SocketConnection sc = (SocketConnection)
-                Connector.open("socket://10.0.53.23:80");
-                sc.setSocketOption(SocketConnection.LINGER, 5);
+                // Create the server listening socket for port 1234
+                ServerSocketConnection scn = (ServerSocketConnection) Connector.open("socket://:1234");
 
-                InputStream is = sc.openInputStream(); 
-                OutputStream os = sc.openOutputStream();
-                
-                
+                // Wait for a connection.
+                SocketConnection sc = (SocketConnection) scn.acceptAndOpen();
 
-                int ch = 0;
-                String string = "";
-                while(ch != -1) { 
-                    ch = is.read();
-                    string += (char) ch;
-                }
-                System.out.println(string);
-                is.close(); os.close(); sc.close();
-                
+                // Set application specific hints on the socket.
+                sc.setSocketOption(SocketConnection.DELAY, 0);
+                sc.setSocketOption(SocketConnection.LINGER, 0);
+                sc.setSocketOption(SocketConnection.KEEPALIVE, 0);
+                sc.setSocketOption(SocketConnection.RCVBUF, 128);
+                sc.setSocketOption(SocketConnection.SNDBUF, 128);
+
+                // Get the input stream of the connection.
+                DataInputStream is = sc.openDataInputStream();
+
+                // Get the output stream of the connection.
+                DataOutputStream os = sc.openDataOutputStream();
+
+                // Read the input data.
+                String result = is.readUTF();
+
+                // Echo the data back to the sender.
+                //os.writeUTF(result);
+
+                // Close everything.
+                is.close();
+                os.close();
+                sc.close();
+                scn.close();
+
             } catch (IOException ex) {
                 System.out.println("Connection Failed!");
             }
@@ -73,18 +88,15 @@ public class Robo2013 extends IterativeRobot {
 
     public void autonomousInit() {
         drive.setSafetyEnabled(false);
-        
-        msg.clearConsole();    
-        
+
+        msg.clearConsole();
+
     }
-    
-   
 
     /**
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
-        
     }
 
     /**
@@ -94,27 +106,21 @@ public class Robo2013 extends IterativeRobot {
         //drive.arcadeDrive(sticky);
         if (driveControls.FOV_Top()) {
             drive.drive(.75, .75);
-        }
-        else if (driveControls.FOV_Bottom()) {
+        } else if (driveControls.FOV_Bottom()) {
             drive.drive(-.75, -.75);
-        }
-        else if (driveControls.FOV_Left()) {
+        } else if (driveControls.FOV_Left()) {
             drive.drive(-.75, .75);
-        }
-        else if (driveControls.FOV_Right()) {
+        } else if (driveControls.FOV_Right()) {
             drive.drive(.75, -.75);
-        }
-        else {
+        } else {
             drive.arcadeDrive(sticky);
         }
-        
+
     }
-    
+
     /**
      * This function is called periodically during test mode
      */
     public void testPeriodic() {
-    
     }
-    
 }
