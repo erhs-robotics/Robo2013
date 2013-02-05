aea0f1a4d935b235054886e52c6d59dba61ef359
Michael Stevens <mls0.5mv2@gmail.com>
2013-02-04 19:32:11 -0500

    work on Neural Network

--- src/org/erhsroboticsclub/robo2013/utilities/neuralNet/NeuronLayer.java
+++ src/org/erhsroboticsclub/robo2013/utilities/neuralNet/NeuronLayer.java
@@ -1,41 +1,94 @@
 package org.erhsroboticsclub.robo2013.utilities.neuralNet;
 
+
+import com.sun.squawk.util.Assert;
+import java.util.Random;
+
 public class NeuronLayer {
 
-	private double[][] weights; //to next layer 
-	private double inputs[];
-	private double outputs[];
-	private int neurons;
-	
-	public NeuronLayer(int neurons, int inputs) {
-		this.inputs = new double[inputs+1];  //With bias
-		this.inputs[0] = 1;
-		this.outputs = new double[neurons];
-		this.weights = new double[neurons][inputs+1];
-		this.neurons=neurons;
-	}
-	
-	public void calcOutputs() {
-		for(int i=0; i<neurons; i++) {
-			double total=0;
-			for(int j=0; j<inputs.length; i++) {
-				total += inputs[j] * weights[i][j];
-			}
-			outputs[i] = Utils.sigmoid(total);
-		}
-	}
-	
-	public double[] getInputs() {return inputs;}
-	public void setInputs(double[] in) {inputs = in;}
-	public void setInput(double in, int index) {inputs[index] = in;}
-	
-	public double[] getOutputs() {return outputs;}
-	public double getOutput(int i) {return outputs[i];}
-	
-	public double[][] getWeights() {return weights;}
-	public void setWeights(double[][] w) {weights = w;}
-	public void setWeight(double w, int i, int j) {weights[i][j] = w;}
-	public double getWeight(int i, int j) {return weights[i][j];}
-	
-	
+    private double[][][] weights; //to next layer
+    private double[][][] prevWeights;
+    private double[][] delta;
+    private double[][] outputs;//The output for each neuron in each layer
+    private int numLayers;
+    private int[] nodesInLayers;
+
+    public NeuronLayer(int[] layers) {
+        nodesInLayers = layers;
+        //init the output for each layer
+        numLayers = layers.length;
+
+        outputs = new double[numLayers][];
+        delta = new double[numLayers][];
+        for (int i = 0; i < numLayers; i++) {
+            outputs[i] = new double[layers[i]];
+            delta[i] = new double[layers[i]];
+        }
+
+        weights = new double[numLayers - 1][][];// -1 for input layer
+        prevWeights = new double[numLayers - 1][][];
+        Random r = new Random();
+        for (int i = 0; i < numLayers - 1; i++) {
+            weights[i] = new double[layers[i + 1]][layers[i] + 1];//+1 for bias
+            for (int j = 0; j < layers[i] + 1; j++) {
+                //assign an init weight from [-0.5, 0.5]
+                weights[i][layers[i + 1] - 1][j] = r.nextDouble() - 0.5;
+            }
+            prevWeights[i] = new double[layers[i + 1]][layers[i]];
+        }
+
+        //check output array
+        Assert.always(outputs.length == layers.length);
+        for (int i = 0; i < layers.length; i++) {
+            Assert.always(outputs[i].length == layers[i]);
+        }
+        //check delta array
+        Assert.always(delta.length == layers.length);
+        for (int i = 0; i < layers.length; i++) {
+            Assert.always(delta[i].length == layers[i]);
+        }
+        //check weights
+        Assert.always(weights.length == layers.length - 1);
+        for (int i = 0; i < layers.length - 1; i++) {
+            Assert.always(weights[i].length == layers[i + 1]);
+            for (int j = 0; j < layers[i+1]; j++) {
+                for (int k = 0; k < layers[i]; k++) {
+                    Assert.always(weights[i][j][k] <= 1);
+                    //System.out.println(weights[i][j][k]);
+                    Assert.always(weights[i][j][k] >= -1);
+                }
+            }
+        }
+    }
+
+    public void calcOutputs(double[] inputs) {
+
+        //place inputs in input layer
+        for (int i = 0; i < nodesInLayers[0]; i++) {
+            outputs[0][i] = inputs[i];
+        }
+
+        //calculate outputs
+        //foreach layer
+        for (int i = 1; i < numLayers; i++) {
+            //foreach node in layer
+            for (int j = 0; j < nodesInLayers[i]; j++) {
+                double sum = 0.0;
+                //foreach weight in node
+                for (int k = 0; k < nodesInLayers[i - 1]; k++) {
+                    sum += outputs[i - 1][k] * weights[i - 1][j][k];
+                }
+                //apply bias
+                sum += weights[i - 1][j][nodesInLayers[i - 1]];
+                outputs[i][j] = Utils.sigmoid(sum);                
+            }
+
+        }
+
+    }
+
+    public double[] getOutputs() {
+        return outputs[numLayers - 1];
+    }
+   
 }
