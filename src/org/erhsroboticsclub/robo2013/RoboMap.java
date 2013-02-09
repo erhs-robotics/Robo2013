f802989f1c88d567bef440649181440b03f4f90b
Michael Stevens <mls0.5mv2@gmail.com>
2013-02-09 12:19:51 -0500

    adding launcher code

--- src/org/erhsroboticsclub/robo2013/RoboMap.java
+++ src/org/erhsroboticsclub/robo2013/RoboMap.java
@@ -7,14 +7,21 @@ public class RoboMap {
     public static final int BOTTOM_LEFT_MOTOR = 2;
     public static final int TOP_RIGHT_MOTOR = 3;
     public static final int BOTTOM_RIGHT_MOTOR = 4;
+    public static final int LOAD_ARM_MOTOR1 = 1;
+    public static final int LOAD_ARM_MOTOR2 = 2;
+    public static final int PRIMARY_LAUNCH_MOTOR = 8; // forward
+    public static final int SECONDARY_LAUNCH_MOTOR = 9; // second
     
     /* Analog */
     public static final int GYRO = 1;
     public static final int ULTRASONIC = 2;
+    public static final int POTENTIOMETER = 3;
     
     /* USB Input */
     public static final int LEFT_DRIVE_STICK = 1;
     public static final int RIGHT_DRIVE_STICK = 2;
     public static final int CONTROL_STICK = 3;
+    
+    
     
 }
