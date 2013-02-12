adedb857008ffbbf31954c4f24d87d53978f2026
Nick Yaculak <nyaculak@gmail.com>
2013-02-12 17:57:33 -0500

    successful serving of images and remote retrieval via http server

--- dashboard/debugServer.py
+++ dashboard/debugServer.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python2
-import sys
+import sys, commands
 sys.path.append('../board/lib')
 from Com import Com
 if len(sys.argv) < 2:
         print "Supply the ip adress in the argument!"
         exit()
 com = Com(sys.argv[1], 80)
+
+# Works on Ubuntu-based systems for sure
+current_ip = commands.getoutput("/sbin/ifconfig").split("wlan0")[1].split("inet addr:")[1].split(" ")[0]
+http_serv_port = ":8000"
+pic_addr = current_ip + http_serv_port + "/target.png"
+
+print pic_addr
 
 while True: 
     try:
@@ -14,7 +21,7 @@ while True:
         com.write_stream('HTTP/1.0 200 OK\r\n')
         com.write_stream("Content-Type: application/json\r\n")
         com.write_stream("Access-Control-Allow-Origin: *\r\n\r\n")
-        com.write_stream('{"status": "Ready", "image" : "http://a57.foxnews.com/global.fncstatic.com/static/managed/img/Scitech/0/371/tardar-sauce-the-cat.jpg", "message" : "yo"}') 
+        com.write_stream('{"status": "Ready", "image" : "%s", "message" : "yo"}' % pic_addr)
         com.close_stream()
         
     except NameError as e:
