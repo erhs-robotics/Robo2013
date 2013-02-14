3bb9854d3fa927052365c753fe0386700289eaa1
Michael Stevens <mls0.5mv2@gmail.com>
2013-02-14 16:04:49 -0500

    adding neural net progress

--- src/org/erhsroboticsclub/robo2013/utilities/neuralNet/NeuronLayer.java
+++ src/org/erhsroboticsclub/robo2013/utilities/neuralNet/NeuronLayer.java
@@ -12,7 +12,7 @@ public class  NeuronLayer{
     private double[][] outputs;//The output for each neuron in each layer
     private int numLayers;
     private int[] nodesInLayers;
-    private final double alpha = 0.05; // learning rate
+    private final double alpha = 0.1; // learning rate
     private final double beta = 0.5; // learning momentum
 
     public NeuronLayer(int[] layers) {
@@ -29,24 +29,24 @@ public class  NeuronLayer{
 
         weights = new double[numLayers][][];
         prevWeights = new double[numLayers][][];
-        
+
         for (int i = 1; i < numLayers; i++) {
-            weights[i] = new double[layers[i]][layers[i-1] + 1];//+1 for bias
-            prevWeights[i] = new double[layers[i]][layers[i-1] + 1];           
+            weights[i] = new double[layers[i]][layers[i - 1] + 1];//+1 for bias
+            prevWeights[i] = new double[layers[i]][layers[i - 1] + 1];
         }
-        
+
         Random r = new Random();
         //init weights
-        for(int i=1;i<numLayers;i++) {
-            for(int j=0;j<outputs[i].length;j++) {
-                for(int k=0;k<weights[i][j].length;k++) {
+        for (int i = 1; i < numLayers; i++) {
+            for (int j = 0; j < outputs[i].length; j++) {
+                for (int k = 0; k < weights[i][j].length; k++) {
                     weights[i][j][k] = r.nextDouble() - 0.5;
-                    prevWeights[i][j][k] = 0;            
+                    prevWeights[i][j][k] = 0;
                 }
             }
         }
 
-        
+
     }
 
     public void calcOutputs(double[] inputs) {
@@ -68,66 +68,66 @@ public class  NeuronLayer{
                 }
                 //apply bias
                 sum += weights[i][j][nodesInLayers[i - 1]];
-                outputs[i][j] = Utils.sigmoid(sum);                
+                outputs[i][j] = Utils.sigmoid(sum);
             }
 
         }
 
     }
-    
+
     public void bpgt(double[] input, double[] target) {
         calcOutputs(input);
         //find delta for output layer
-        for(int i=0;i<nodesInLayers[numLayers-1];i++) {
-            double y = outputs[numLayers-1][i]; //the output of the node
+        for (int i = 0; i < nodesInLayers[numLayers - 1]; i++) {
+            double y = outputs[numLayers - 1][i]; //the output of the node
             // y * (1-y) is f'(x) and (target[i] - y) is the error
-            delta[numLayers-1][i] = y * (1 - y) * (target[i] - y);
+            delta[numLayers - 1][i] = y * (1 - y) * (target[i] - y);
         }
-        
+
         //find delta for hidden layers
         //i=numLayers-2 because output and input layers are not hidden layers
         //for each layer
-        for(int i=numLayers-2;i>0;i--) {
+        for (int i = numLayers - 2; i > 0; i--) {
             //for each node in layer
-            for(int j=0;j<nodesInLayers[i];j++) {
+            for (int j = 0; j < nodesInLayers[i]; j++) {
                 double blame = 0.0;
                 //for each wieght connecting the node to the next layer
-                for(int k=0;k<nodesInLayers[i+1];k++) {
-                    blame += delta[i+1][k] * weights[i+1][k][j];
+                for (int k = 0; k < nodesInLayers[i + 1]; k++) {
+                    blame += delta[i + 1][k] * weights[i + 1][k][j];
                 }
-                delta[i][j] = outputs[i][j] * (1-outputs[i][j]) * blame;
+                delta[i][j] = outputs[i][j] * (1 - outputs[i][j]) * blame;
             }
         }
-        
+
         //apply momentum
-        for(int i=1;i<weights.length;i++) {
-            for(int j=0;j<weights[i].length;j++) {
-                for(int k=0;k<weights[i][j].length;k++) {
+        for (int i = 1; i < weights.length; i++) {
+            for (int j = 0; j < weights[i].length; j++) {
+                for (int k = 0; k < weights[i][j].length; k++) {
                     weights[i][j][k] -= alpha * prevWeights[i][j][k];
-                }                
+                }
             }
         }
-        
+
         //adjust weights
-        for(int i=1;i<weights.length;i++) {
-            for(int j=0;j<weights[i].length;j++) {
-                for(int k=0;k<nodesInLayers[i-1];k++) {
-                    prevWeights[i][j][k] = beta*delta[i][j] * outputs[i-1][k];
+        for (int i = 1; i < weights.length; i++) {
+            for (int j = 0; j < weights[i].length; j++) {
+                for (int k = 0; k < nodesInLayers[i - 1]; k++) {
+                    prevWeights[i][j][k] = beta * delta[i][j] * outputs[i - 1][k];
                     weights[i][j][k] += prevWeights[i][j][k];
                 }
             }
         }
-        
-        
-        
-        
+
+
+
+
     }
-    
+
     public double mse(double[] targets) {
         double mse = 0;
-        for(int i=0;i<targets.length;i++) {
-            mse += (targets[i] - outputs[outputs.length-1][i])
-                   * (targets[i] - outputs[outputs.length-1][i]);
+        for (int i = 0; i < targets.length; i++) {
+            mse += (targets[i] - outputs[outputs.length - 1][i])
+                    * (targets[i] - outputs[outputs.length - 1][i]);
         }
         return mse / 2;
     }
@@ -136,47 +136,50 @@ public class  NeuronLayer{
         return outputs[numLayers - 1];
     }
 
+    public double[][][] getWeights() {
+        return weights;
+    }
+
     public static void main(String[] args) {
 
-        int[] layers = {2, 7, 5, 1};       
+        int[] layers = {2, 7, 7, 1};
         NeuronLayer nn = new NeuronLayer(layers);
-        double[][][] data = {{{1, 1}, {0}},
-                             {{1, 0}, {1}},
-                             {{0, 1}, {1}},
-                             {{0, 0}, {0}}};
+        double[][][] data = {{{0, 0}, {0}},
+                            {{0, 1}, {1}},
+                            {{1, 0}, {1}},
+                            {{1, 1}, {0}}};
         // Train the nn
         double mse = 0.11;
         int i = 0;
-        while(mse > 0.001){
+        while (mse > 0.084) {
             double ave_mse = 0;
-            for(int j=0;j<data.length;j++) {
+            for (int j = 0; j < data.length; j++) {
                 nn.bpgt(data[j][0], data[j][1]);
                 ave_mse += nn.mse(data[j][1]);
-                
-                
+
+
             }
             ave_mse /= data.length;
             mse = ave_mse;
-            
+
             System.out.print("MSE: ");
             System.out.println(mse);
             i++;
-        } 
+        }
         System.out.print("Trained in ");
         System.out.print(i);
         System.out.println(" iterations");
-        
+
         // Test the nn
-        for(int j=0;j<data.length;j++) {
+        for (int j = 0; j < data.length; j++) {
             nn.calcOutputs(data[j][0]);
             System.out.print("Input: ");
             //System.out.println(Arrays.deepToString(data[j]));
             System.out.print("Output:");
-            System.out.println(nn.getOutputs()[0]);                               
+            System.out.println(nn.getOutputs()[0]);
         }
         
-        
-        
+
 
     }
 }
