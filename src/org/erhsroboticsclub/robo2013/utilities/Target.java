99b31f11b061fa9c4125080ce2746e7600d2e299
Michael Stevens <mls0.5mv2@gmail.com>
2013-02-15 18:41:38 -0500

    adding target class

---
+++ src/org/erhsroboticsclub/robo2013/utilities/Target.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.erhsroboticsclub.robo2013.utilities;
+
+/**
+ *
+ * @author michael
+ */
+public class Target {
+    public double x, distance, height;  
+}
