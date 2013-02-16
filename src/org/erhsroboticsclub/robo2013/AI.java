7ea57433407ed96e00181e4f7acba719b5c4c30d
Michael Stevens <mls0.5mv2@gmail.com>
2013-02-16 11:20:05 -0500

    initial turn to target code

--- src/org/erhsroboticsclub/robo2013/AI.java
+++ src/org/erhsroboticsclub/robo2013/AI.java
@@ -1,40 +1,60 @@
 package org.erhsroboticsclub.robo2013;
 
+import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.networktables2.util.List;
 import java.util.Hashtable;
+import org.erhsroboticsclub.robo2013.utilities.MathX;
 import org.erhsroboticsclub.robo2013.utilities.Messenger;
+import org.erhsroboticsclub.robo2013.utilities.PIDControllerX;
 import org.erhsroboticsclub.robo2013.utilities.Target;
 
 public class AI {
-    
+
     private RobotDrive drive;
     private Messenger msg;
     private Com com;
-    
+    private PIDControllerX pid;
+
     public AI(RobotDrive drive) {
         this.drive = drive;
+        pid = new PIDControllerX(1, 0, 10);
+
     }
+
     private List getAllTargets() {
         Hashtable table = com.getValues("crio");
-        return com.parseTargets((String)table.get("targets"));        
+        return com.parseTargets((String) table.get("targets"));
     }
-    
-    public Target getTopTarget() {        
+
+    public Target getTopTarget() {
         List targets = getAllTargets();
-        Target high = (Target)targets.get(0);
-        for(int i=0;i<targets.size();i++) {
-            Target t = (Target)targets.get(i);
-            if(t.height > high.height) {
+        Target high = (Target) targets.get(0);
+        for (int i = 0; i < targets.size(); i++) {
+            Target t = (Target) targets.get(i);
+            if (t.height > high.height) {
                 high = t;
-            }        
+            }
         }
-        
-        return high;        
+
+        return high;
     }
-    
-    public void turnToTarget() {
+
+    public void turnToTarget(int t) {
+        pid.setSetpoint(320);
         
+        Target target;
+        
+
+        do {
+            List list = getAllTargets();
+            target = (Target) list.get(t);
+            double correction = pid.doPID(target.x);
+            drive.tankDrive(correction, -correction);
+            
+        } while(!MathX.isWithin(target.x, 320, 7));
+
+
+
     }
-    
 }
