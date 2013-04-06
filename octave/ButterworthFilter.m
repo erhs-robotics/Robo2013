77e8bc9575bf970a19779036b5dd2e72d9cb0d21
Nick Yaculak <nyaculak@gmail.com>
2013-04-06 00:02:11 -0400

    simulating launcher more

--- octave/ButterworthFilter.m
+++ octave/ButterworthFilter.m
@@ -10,7 +10,7 @@ nyq_freq2 = oversamp_freq/2;
 
 % cut off frequency, in Hz, between 0 and nyquist frequency
 % try playing around and changing this value guys
-cutoff_freq=5;
+cutoff_freq=7;
 
 % create a first-order Butterworth low pass.
 % The returned vectors are of legth n.
@@ -20,7 +20,7 @@ cutoff_freq=5;
 % create a 5 seconds signal with 3 parts:
 % a 1 Hz and a 20 Hz sine wave and some gaussian noise.
 t=0:1/samp_freq:5;
-input=sin(2*pi*t) + sin(2*pi*20*t) + randn(size(t));
+input=20 + sin(2*pi*20*t) + randn(size(t));
 output=filter(b,a,input);
 plot(t, [output]);
 print("normsamp");
@@ -30,7 +30,7 @@ print("normsamp");
 
 % oversample time interval
 t2=0:1/oversamp_freq:5;
-input2=sin(2*pi*t2) + sin(2*pi*200*t2) + randn(size(t2));
+input2=20 + sin(2*pi*200*t2) + randn(size(t2));
 output2=filter(b,a,input2);
 plot(t2, [output2]);
 print("oversamp");
